e04278232206aed60185cddaaa02c0b8a74cf7d5
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2020-12-26 02:37:04 +0900

    Update index.js

--- vanila-js/index.js
+++ vanila-js/index.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 // 아이디 선택하기 및 제목 바꾸기
 // document.getElementByID(""); 사용한다.
 const title = document.getElementById("title");
-
+// 타이틀 콘솔에 보기
 console.log(title);
-=======
+
 // 제목바꾸기
-const title = document.getElementById("title");
 title.innerHTML = "Hi From JS";
->>>>>>> gh-pages
