6387bc4b48d67c2484b19a93b56f3a7b3458df80
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2020-12-27 00:24:11 +0900

    move the contents what I studied to study.js

--- vanila-js/study.js
+++ vanila-js/study.js
@@ -67,3 +67,35 @@ console.log(multiply);
 console.log(divide);
 console.log(minus);
 console.log(power);
+
+// 아이디 선택하기 및 제목 바꾸기
+// document.getElementByID(""); 사용한다.
+// const title = document.getElementById("title");
+
+//querySelector 이용하기
+const title = document.querySelector("#title");
+
+// 타이틀 콘솔에 보기
+console.log(title);
+
+// 타이틀이 가진 함수 콘솔로 보기
+// console.dir(title);
+
+// 제목바꾸기
+title.innerHTML = "Hi From JS";
+//색 바꾸기
+title.style.color = "red";
+
+// title바꾸기
+document.title = "Wow";
+
+// event 발생할때마다 로그에 보여주기
+function handleResize(event) {
+  console.log(event);
+}
+
+// 이벤트 가로채기
+// window resize는 handleResize를 호출하는거임!
+// handleResize라고 적어야한다. 그래야 우리가 원하는 시간에 호출할 수 있음
+// handleResize()라고 적으면 지금 바로 호출하라는거임.
+window.addEventListener("resize", handleResize);
