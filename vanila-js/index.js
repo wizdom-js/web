030cb09952b0bd8b83ec896df368f369e660dfa0
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2020-12-27 00:24:38 +0900

    added event

--- vanila-js/index.js
+++ vanila-js/index.js
@@ -1,20 +1,9 @@
-// 아이디 선택하기 및 제목 바꾸기
-// document.getElementByID(""); 사용한다.
-// const title = document.getElementById("title");
-
-//querySelector 이용하기
 const title = document.querySelector("#title");
 
-// 타이틀 콘솔에 보기
-console.log(title);
+// title을 클릭할때마다 제목 색깔을 빨간색으로 바꾸기
+function handleClick() {
+  title.style.color = "red";
+}
 
-// 타이틀이 가진 함수 콘솔로 보기
-// console.dir(title);
-
-// 제목바꾸기
-title.innerHTML = "Hi From JS";
-//색 바꾸기
-title.style.color = "red";
-
-// title바꾸기
-document.title = "Wow";
+//title은 click 이벤트를 기다리고 있게
+title.addEventListener("click", handleClick);
