2c42628edb6643eaa2c284a1b3a612be5bc00a71
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-06-07 20:46:37 +0900

    스프링으로 전환

--- spring/core/src/main/java/hello/core/AppConfig.java
+++ spring/core/src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration // 설정 정보. 구성 정보를 담당
 // 애플리케이션 전체를 설정하고 구성한다는 뜻
 public class AppConfig {
 
+    @Bean // 입력하면 귤색들이 스프링 컨테이너에 등록된다.
     // 생성자 주입 : 생성자를 통해서 객체가 new 인스턴스 생성된게 들어간다
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+   public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- spring/core/src/main/java/hello/core/MemberApp.java
+++ spring/core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        // 스프링 버전
+        // 스프링은 모든게 applicationcontext 부터 시작한다. 스프링 컨테이너임
+        // 모든걸 관리해줌 @Bean 같은거
+        // annotationconfig~ appconfig의 configuration을 관리한다.
+        // 이렇게하면 appconfig의 환경설정 정보를 가지고 안에 있는 아이들을(@Bean들) 스프링 빈에다 스프링 컨테이너 객체 생성한거에다가 다 넣어주고 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 꺼낼거 이름(메소드 이름 / 나는 이 객체를 찾을거야) , 반환타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         // Long 타입이기 떄문에 1L로 한다. 안붙이면 컴파일 오류남
         Member member = new Member(1L, "memberA", Grade.VIP);
