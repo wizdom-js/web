64c237ef4c9a89df92d3823a421a197cac27f5d5
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-06-22 13:36:35 +0900

    http 응답 데이터 - 단순 텍스트, html

---
+++ spring/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ResponseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("Kim");
+        helloData.setAge(20);
+
+        // 이런식으로 바꿀거임
+        // {"username":"Kim", "age":20}
+        String result = objectMapper.writeValueAsString(helloData); // 객체로 값을 써써 문자로바꿔라 뜻
+        response.getWriter().write(result);
+
+    }
+}
