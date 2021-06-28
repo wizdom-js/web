d8ff2cc570ffd5bd972991291e948d8a9e80d6dc
wizdom-js <65435762+wizdom-js@users.noreply.github.com>
2021-06-28 14:22:21 +0900

    MVC 패턴 - 적용

--- spring/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ spring/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -10,26 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "MvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
-public class MvcMemberSaveServlet extends HttpServlet {
+@WebServlet(name = "MvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
 
-        // 이전 서블릿 코드랑 같음
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+        // 모델에 담기
+        request.setAttribute("members", members); //
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
-
-        // Model에 데이터를 보관한다.
-        request.setAttribute("member", member);
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
--- spring/servlet/src/main/webapp/WEB-INF/views/save-result.jsp
+++ spring/servlet/src/main/webapp/WEB-INF/views/save-result.jsp
@@ -7,10 +7,9 @@
 <body>
 성공
 <ul>
-
-      <li>id=${member.id}</li>
-      <li>username=${member.username}</li>
-      <li>age=${member.age}</li>
+  <li>id=${member.id}</li>
+  <li>username=${member.username}</li>
+  <li>age=${member.age}</li>
 </ul>
 <a href="/index.html">메인</a>
 </body>
